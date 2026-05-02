d8c5f7bac08f9922d2025c8076ab3df209adf78e
ibtissamoldi <ibtissamoldi@gmail.com>
2026-05-02 15:32:23 +0200

    only variables in civilization class

--- M3/game/Civilization.java
+++ M3/game/Civilization.java
@@ -1,5 +1,25 @@
 package M3.game;
 
+import java.util.ArrayList;
+
+import M3.interfaces.MilitaryUnit;
+
 public class Civilization {
-    
+    private int technologyDefense;
+	private int technologyAtack;
+	
+	private int wood;
+	private int iron;
+	private int food;
+	private int mana;
+
+	private int magicTower;
+	private int church;
+	private int farm;
+	private int smithy;
+	private int carpentry;
+
+	private int battles;
+	private ArrayList<MilitaryUnit>[] army = new ArrayList[9];
+	
 }
